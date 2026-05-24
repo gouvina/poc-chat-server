a1fdecb6ab344b7c6eef0f4678a34cd550b776fd
franciscocabrera <cabrerafernandez.francisco@gmail.com>
2026-05-24 19:19:59 -0300

    crated message entity

---
+++ src/message/message.entity.ts
@@ -0,0 +1,18 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuidv4 } from 'uuid';
+
+
+@Entity('messages')
+export class Message {
+    @PrimaryColumn({type: 'char', length: 36})
+    id: string = uuidv4()
+
+    @Column({type: 'varchar'})
+    content!: string
+
+    @Column({ type: 'varchar'})
+    sender!: string
+
+    @CreateDateColumn({ type: 'timestamp with time zone'})
+    sentAt!: Date
+}
