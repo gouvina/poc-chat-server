8e6063617a83f87caceb730a8d0c5657ffe259ff
Francisco <cabrerafernandez.francisco@gmail.com>
2026-09-05 16:34:23 -0300

    fixed issues with password decoding twice and refresh token

--- src/auth/dto/refresh.dto.ts
+++ src/auth/dto/refresh.dto.ts
@@ -1,7 +1,11 @@
+import { IsString } from "class-validator";
+
 export class RefreshTokenDto {
+    @IsString()
     refreshToken!: string;
   }
 
-export class RefreshResponseDto {  
+export class RefreshResponseDto {
+    @IsString()
     accessToken!: string;
   }
